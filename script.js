6ba3fa4046e519e45095a9bf09536167c3ea9d75
capibara123406 <kewyn.goncalves@escola.pr.gov.br>
2025-11-28 09:39:23 -0300

    e

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-const aI = document.getElementById('a');
-const bI = document.getElementById('b');
-const btn = document.getElementById('btn');
-const Saida = document.getElementById('rst');
+const ai = document.getElementById('a');//Renda
+const bi = document.getElementById('b');//Score
+const btn = document.getElementById('btn');//Button
+const Saida = document.getElementById('rst');//Resultado
+
 btn.onclick = function () {
-    let base = 30;
-    const aa = +aI.value;
-    const bb = bI.value;
-    if (aa >= 150 && bb >= 200) {
-        base += ((a * 0.10) * bb);
-        Saida.textContent = 'Total: ' + base;
-    } else if (aa >= 250 && bb >= 330) {
-        base += ((((aa * 1.54) * 0.58)(bb * 2.34) + 20));
-        Saida.textContent = 'Total: ' + base;
+   
+    const a = ai.value;
+    const b = bi.value;
+
+    let EXCED = 0;
+    let Total = 0;
+
+    let Mensagem = [];
+
+    if (a >= 80) {
+        EXCED = ((a - 80) * 2);
+
+        Mensagem.push('GB Excedentes (taxa aplicada)');
     } else {
-        Saida.textContent = 'Total: ' + base;
+        Mensagem.push('Sem GB execedente');
     }
+
+    if (b === 's') {
+        Total = EXCED * 0.15;
+        EXCED = Total - EXCED;
+
+        Mensagem.push('Bonus aplicado');
+    } else {
+        Mensagem.push('Sem Bônus');
+    }
+
+    if (a >= 100) {
+         EXCED = (((a - 80) * 2) + 20);
+
+        Mensagem.push('GB Excedentes (taxa aplicada) + Multa de R$20');
+    } else {
+        Mensagem.push('Sem multa');
+    }
+
+    Mensagem.push(`<strong>Total: ${EXCED.toFixed(2)}</strong>`);
+    Mensagem.push(`<strong>Total: ${Total.toFixed(2)}</strong>`);
+
+    Saida.innerHTML = Mensagem.join('<br>');
+
 }
